d86169fcbc8e2214c59e81c6e8d9a0b8a100eee7
Jade Pennig <jade@pennig.name>
2019-08-20 16:37:13 -0500

    Add screener module and config (successful tests locally)

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,7 @@
 import { configure } from '@storybook/react';
 
 configure(require.context('../ui', true, /stories\.js$/), module);
+
+if (typeof window === 'object') {
+  window.__screener_storybook__ = require('@storybook/react').getStorybook;
+}
---
+++ screener.config.js
@@ -0,0 +1,11 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+module.exports = {
+  projectRepo: 'zahnster/storybook-docs',
+  storybookConfigDir: '.storybook',
+  apiKey: process.env.SCREENER_KEY,
+  resolution: '1024x768',
+  storybookStaticBuildDir: '.storybook-static'
+};
