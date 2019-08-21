c9b39b35f622f3b14cf1a2298ccba4f26f0e494c
Jade Pennig <jade@pennig.name>
2019-08-21 14:23:48 -0500

    Adding a11y addon.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,12 @@
+// Copyright (c) 2015-present, salesforce.com, inc. All rights reserved
+// Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license
+
 import '@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.css';
 import './styles.css';
 
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { withA11y } from '@storybook/addon-a11y';
+
+addDecorator(withA11y);
 
 configure(require.context('../ui', true, /stories\.js$/), module);
--- .storybook/presets.js
+++ .storybook/presets.js
@@ -1,1 +1,4 @@
+// Copyright (c) 2015-present, salesforce.com, inc. All rights reserved
+// Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license
+
 module.exports = ['@storybook/addon-docs/react/preset'];
