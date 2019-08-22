ccdc87b22cd0bd572ee6d0800512f1dc0321ce9e
Jade Pennig <jade@pennig.name>
2019-08-22 14:18:46 -0500

    gonna get one of these working...

--- screener.config.js
+++ screener.config.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 module.exports = {
   projectRepo: 'zahnster/storybook-docs',
   storybookConfigDir: '.storybook',
+  storybookStaticDir: 'node_modules/@salesforce-ux/design-system',
   apiKey: process.env.SCREENER_KEY,
   resolution: '1024x768',
   baseBranch: 'master',
