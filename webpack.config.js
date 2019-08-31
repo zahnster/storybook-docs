0ef3ce50b2bf8d50a87966034f7f7e7f88120c91
Jade Pennig <jade@pennig.name>
2019-08-31 17:05:17 -0500

    packed

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,20 @@
 const path = require('path');
+const glob = require('glob');
 
 module.exports = {
-  entry: './docs.js',
-  // target: 'node',
+  entry: glob.sync('./ui/components/**/docs.stories.mdx'),
+
   output: {
-    libraryTarget: 'commonjs2',
+    libraryTarget: 'commonjs',
     filename: '[name].js',
-    // chunkFilename: '[name].bundle.js',
     path: path.resolve(__dirname, '.dist/docs')
   },
+
   externals: {
-    react: {
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'React',
-      root: 'React'
-    },
-    'react-dom': {
-      commonjs: 'react-dom',
-      commonjs2: 'react-dom',
-      amd: 'ReactDOM',
-      root: 'ReactDOM'
-    }
-    // ['@mdx-js/react']: '@mdx-js/react'
+    react: 'react',
+    'prop-types': 'propTypes'
   },
-  optimization: {
-    minimize: false
-  },
+
   module: {
     rules: [
       {
@@ -34,17 +22,12 @@ module.exports = {
         exclude: /node_modules/,
         use: ['babel-loader']
       },
+
       {
         test: /\.mdx$/,
         exclude: /node_modules/,
         use: ['babel-loader', '@mdx-js/loader']
       }
     ]
-  },
-  resolve: {
-    alias: {
-      react: path.resolve(__dirname, './node_modules/react'),
-      'react-dom': path.resolve(__dirname, './node_modules/react-dom')
-    }
   }
 };
